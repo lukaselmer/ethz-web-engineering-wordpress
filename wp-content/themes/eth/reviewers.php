1c0eab3f3acfecfb17fc3cae6e5800ed554e65da
Lukas Elmer <lukas.elmer@gmail.com>
2014-04-13 07:09:03 +0200

    reviewers template

--- wp-content/themes/eth/reviewers.php
+++ wp-content/themes/eth/reviewers.php
@@ -1,64 +1,50 @@
+<?php
+/**
+ * Template Name: List the reviewers
+ */
+
+get_header();
+
+$loop = new WP_Query(array('post_type' => 'reviewer', 'orderby' => 'meta_value', 'meta_key' => 'relevance'));
+?>
+
 <article class="reviewers">
     <h2><a href="#">Evaluate Thy Hotel</a></h2>
 
+
     <div class="reviewer-profiles">
-        <div class="reviewer-profile">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/reviewers/1.png" alt=""/>
 
-            <div class="attributes">
-                <div class="name">Mr. Anderson</div>
-                <div class="title">CEO</div>
-                <div class="role">Main Reviewer</div>
-                <div class="cite">I will show you a world in which every trip is possible..</div>
+        <?php
+        while ($loop->have_posts()) : $loop->the_post();
+            $custom_fields = get_post_custom();
+            ?>
+            <div class="reviewer-profile">
+                <?php if (has_post_thumbnail()) {
+                    $str = get_the_post_thumbnail(get_the_ID(), 'full');
+                    $str = preg_replace('/width=\"\d+\"/', '', $str);
+                    $str = preg_replace('/height=\"\d+\"/', '', $str);
+                    $str = preg_replace('/attachment-full /', '', $str);
+                    echo $str;
+                } ?>
+
+                <div class="attributes">
+                    <div class="name"><?php the_title() ?></div>
+                    <div class="title"><?php echo $custom_fields['title'][0] ?></div>
+                    <div class="role"><?php echo $custom_fields['role'][0] ?></div>
+                    <div class="cite"><?php echo $custom_fields['cite'][0] ?></div>
+                </div>
             </div>
-        </div>
-        <div class="reviewer-profile">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/reviewers/3.png" alt=""/>
+        <?php
+        endwhile; ?>
 
-            <div class="attributes">
-                <div class="name">Mr. Smith</div>
-                <div class="title">Board Member</div>
-                <div class="role">Main Reviewer</div>
-                <div class="cite">That's what humanity does... travels.</div>
-            </div>
-        </div>
-
-        <div class="reviewer-profile">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/reviewers/2.png" alt=""/>
-
-            <div class="attributes">
-                <div class="name">Ms. Trinity</div>
-                <div class="role">Assistant Reviewer</div>
-                <div class="cite">I love the chosen ones, so we love sending people to the chosen places!</div>
-            </div>
-        </div>
-        <div class="reviewer-profile">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/reviewers/4.png" alt=""/>
-
-            <div class="attributes">
-                <div class="name">Mr. Morpheus</div>
-                <div class="role">Assistant Reviewer</div>
-                <div class="cite">Every man has the keys to the fun... start travelling!</div>
-            </div>
-        </div>
-
-        <div class="reviewer-profile">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/reviewers/5.png" alt=""/>
-
-            <div class="attributes">
-                <div class="name">Mr. Mouse</div>
-                <div class="role">Draft Optimiser</div>
-                <div class="cite">You need more! Refusing travelling is refusing the only thing that makes us human.</div>
-            </div>
-        </div>
-        <div class="reviewer-profile">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/reviewers/6.png" alt=""/>
-
-            <div class="attributes">
-                <div class="name">Mr. Tank</div>
-                <div class="role">Administrative Assistant</div>
-                <div class="cite">There's always a way out.. of your stress! You just have to reach that fast! Start travelling!</div>
-            </div>
-        </div>
     </div>
 </article>
+
+<?php
+/**
+ * Template Name: List the reviewers
+ */
+get_sidebar();
+get_footer();
+
+?>
