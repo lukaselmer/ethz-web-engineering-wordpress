d8d66e5bd1a60cb7ca1db7cf95da76c726b9a61c
Lukas Elmer <lukas.elmer@gmail.com>
2014-04-13 05:37:11 +0200

    single page, index page, thumbnails, menus

--- wp-content/themes/eth/functions.php
+++ wp-content/themes/eth/functions.php
@@ -8,3 +8,4 @@ add_action( 'wp_enqueue_scripts', 'eth_scripts' );
 
 register_nav_menu( 'top', 'Top Menu' );
 register_nav_menu( 'left', 'Left Menu' );
+add_theme_support( 'post-thumbnails' );
--- wp-content/themes/eth/index.php
+++ wp-content/themes/eth/index.php
@@ -1,15 +1,29 @@
-<?php get_header(); ?>
+<?php get_header();
 
-<?php
-$r = mt_rand(0, 2);
-if ($r == 0)
-    include("reviews.php");
-elseif ($r == 1)
-    include("weekly.php");
-else
-    include("reviewers.php");
-?>
+while (have_posts()) : the_post(); ?>
+    <article class="hotel-item">
+        <div>
+            <h3><?php the_title() ?></h3>
+            <?php if (has_post_thumbnail()) {
+                $str = get_the_post_thumbnail(get_the_ID(), 'full');
+                $str = preg_replace('/width=\"\d+\"/', '', $str);
+                $str = preg_replace('/height=\"\d+\"/', '', $str);
+                $str = preg_replace('/attachment-full /', '', $str);
+                echo $str;
+            } ?>
 
-<?php get_sidebar(); ?>
+            <p><?php the_content() ?></p>
 
-<?php get_footer(); ?>
+            <div><a class="more" href="<?php the_permalink() ?>">Read more...</a></div>
+
+        </div>
+        <div class="author">
+            <a href="#"><?php the_date() ?> by <?php the_author() ?></a>
+            <!--<span>TODO views</span>-->
+            <?php edit_post_link(); ?>
+        </div>
+    </article>
+<?php endwhile;
+
+get_sidebar();
+get_footer();
