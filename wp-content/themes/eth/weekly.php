55da4fdf2b77ccabd14cf9aa96673048278a9d66
Lukas Elmer <lukas.elmer@gmail.com>
2014-04-14 01:49:55 +0200

    Dynamic weekly

--- wp-content/themes/eth/weekly.php
+++ wp-content/themes/eth/weekly.php
@@ -6,8 +6,18 @@
 get_header();
 
 ?>
+<?php while (have_posts()) : the_post(); ?>
     <article class="weekly">
-        <h3><a href="#">ETH suggests..</a></h3>
+
+        <h3><a href="#"><?php the_title() ?></a></h3>
+
+        <?php
+        $ids = get_post_gallery($post, false)['ids'];
+        $arr = explode(',', $ids);
+        $imageId = $arr[array_rand($arr)];
+        $image_url = wp_get_attachment_url($imageId);
+        if (!isset($image_url)) $image_url = get_template_directory_uri() . '/images/sydney-harbour-panorama1bl.jpg';
+        ?>
 
         <div class="image-slider">
             <div class="controls">
@@ -19,49 +29,50 @@ get_header();
                 <a href="#zoomIn" class="big">+</a>
                 <a href="#zoomOut" class="big">-</a>
             </div>
-            <div class="cropped"><img src="<?php echo get_template_directory_uri(); ?>/images/sydney-harbour-panorama1bl.jpg"/></div>
+            <div class="cropped"><img src="<?php echo $image_url ?>"/></div>
             <div class="thumbnail">
                 <div class="thumbnail-inner">
                     <div class="rectangle"></div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/sydney-harbour-panorama1bl-thumbnail.jpg"/>
+                    <img src="<?php echo $image_url ?>"/>
                 </div>
             </div>
         </div>
 
+        <?php
+        function disable_auto_gallery($content, $atts) { return '&nbsp;'; }
 
-        <?php while (have_posts()) : the_post();
-            the_content();
-            edit_post_link();
-            $custom_fields = get_post_custom();
+        add_filter('post_gallery', 'disable_auto_gallery', 10, 2);
 
-            $cite = $custom_fields['cite'][0];
-            if(!isset($cite)) update_post_meta($post->ID, 'cite', '"A life without travel is a life unlived"');
 
-            $author = $custom_fields['author'][0];
-            if(!isset($author)) update_post_meta($post->ID, 'author', 'Mr. Anderson');
-            ?>
+        the_content();
+        edit_post_link();
+        $custom_fields = get_post_custom();
 
-            <blockquote>
-                <?php echo $cite ?>
+        $cite = $custom_fields['cite'][0];
+        if (!isset($cite)) update_post_meta($post->ID, 'cite', '"A life without travel is a life unlived"');
 
-                <div>
-                    <?php if (has_post_thumbnail()) {
-                        $str = get_the_post_thumbnail(get_the_ID(), 'full');
-                        $str = preg_replace('/width=\"\d+\"/', '', $str);
-                        $str = preg_replace('/height=\"\d+\"/', '', $str);
-                        $str = preg_replace('/attachment-full /', '', $str);
-                        echo $str;
-                    } ?>
+        $author = $custom_fields['author'][0];
+        if (!isset($author)) update_post_meta($post->ID, 'author', 'Mr. Anderson');
+        ?>
 
-                    <div><?php echo $author ?></div>
-                </div>
-            </blockquote>
-        <?php endwhile; ?>
+        <blockquote>
+            <?php echo $cite ?>
 
+            <div>
+                <?php if (has_post_thumbnail()) {
+                    $str = get_the_post_thumbnail(get_the_ID(), 'full');
+                    $str = preg_replace('/width=\"\d+\"/', '', $str);
+                    $str = preg_replace('/height=\"\d+\"/', '', $str);
+                    $str = preg_replace('/attachment-full /', '', $str);
+                    echo $str;
+                } ?>
+
+                <div><?php echo $author ?></div>
+            </div>
+        </blockquote>
 
     </article>
-
-<?php
+<?php endwhile;
 
 get_sidebar();
 get_footer();
